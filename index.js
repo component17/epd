2d58ea3c31034ccad8950935188c05f1fdbc8456
seapp88 <seapp88@icloud.com>
2019-08-06 23:53:01 +0300

    test

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const refreshDisplay = message =>
             img.rectangle(1, 1, 43, 45, epd.colors.black);
 
             img.stringFT(epd.colors.black, mdi, 24, 0,
-                8,
+                6,
                 36,
                 '');
 
