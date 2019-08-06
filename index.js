d0b5d22aa3e134a4f2446341644c98d9815b1a9e
seapp88 <seapp88@icloud.com>
2019-08-07 00:34:54 +0300

    test

--- index.js
+++ index.js
@@ -57,13 +57,13 @@ const refreshDisplay = message =>
                 Math.round(height / 2 + (box2[1] - box2[7]) / 2),
                 '192.168.1.77:3000');
 
-            let box3 = img.stringFTBBox(epd.colors.black, font, 14, 0, 0, 0, 'Powered by Component17');
+            let box3 = img.stringFTBBox(epd.colors.black, font, 14, 0, 0, 0, 'Powered \n by Component17');
 
             // // Center the message
             img.stringFT(epd.colors.black, font, 14, 0,
                 Math.round(width / 2 - (box3[4] - box3[6]) / 2 + 25),
                 Math.round(height / 2 + (box3[1] - box3[7]) / 2 + 50),
-                'Powered by Component17');
+                'Powered \n by Component17');
 
 
 
